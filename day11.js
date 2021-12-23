55f9bf9430296ea643c7fd1ac38ea136338361a9
siddhant gore <siddhantgore111@gmail.com>
2021-12-24 00:14:34 +0530

    day11

---
+++ day11.js
@@ -0,0 +1,19 @@
+//Write a program for factoria of number
+
+const number = parseInt(prompt('Enter a positive integer: '));
+
+if (number < 0) {
+    console.log('Error! Factorial for negative number does not exist.');
+}
+
+else if (number === 0) {
+    console.log("The factorial of 0 is 1");
+}
+
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial of ${number} is ${fact}.`);
+}
